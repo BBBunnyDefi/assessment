d8c505d492b27b2a7aac0902c8610d3a0dad05e9
BBBunny Dev <BBBunny@dev.local>
2023-01-02 20:23:35 +0700

    create func CreateExpensesHandler & TestCreateExpensesHandler

--- rest/expenses/expenses.go
+++ rest/expenses/expenses.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 )
 
 type expenses struct {
@@ -29,4 +30,31 @@ type Err struct {
 
 func (e *expenses) HealthHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Server health OKKK")
+}
+
+// EXP01: POST /expenses - with json body
+func (e *expenses) CreateExpensesHandler(c echo.Context) error {
+	// Query all
+	exps := Expenses{}
+	err := c.Bind(&exps)
+	if err != nil {
+		// log.Fatal("can't insert data", err)
+		return c.JSON(http.StatusBadRequest, Err{Message: "can't Bind : " + err.Error()})
+	}
+
+	// fmt.Println("##### dump pq.Array(exps.Tags) #####")
+	// fmt.Printf("%T\n", pq.Array(exps.Tags))
+	// fmt.Println(pq.Array(exps.Tags))
+	// fmt.Println("##### dump pq.Array(exps.Tags) #####")
+
+	row := e.DB.QueryRow("INSERT INTO expenses (title, amount, note, tags) VALUES ($1, $2, $3, $4) RETURNING id", exps.Title, exps.Amount, exps.Note, pq.Array(exps.Tags))
+	err = row.Scan(&exps.ID)
+
+	// fmt.Println(exps)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan : " + err.Error()})
+	}
+
+	return c.JSON(http.StatusCreated, exps)
 }
