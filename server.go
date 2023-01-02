38c09ac66c739f19326f0fc89bff8146bf444a7a
BBBunny Dev <BBBunny@dev.local>
2023-01-02 19:32:39 +0700

    create simple server

--- server.go
+++ server.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/BBBunnyDefi/assessment/rest/expenses"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	h := expenses.NewApp(nil)
+
+	// use echo lib for server
+	e := echo.New()
+
+	e.GET("/", h.HealthHandler)
+
+	serverPort := ":2565"
+	e.Logger.Fatal(e.Start(serverPort))
 }
